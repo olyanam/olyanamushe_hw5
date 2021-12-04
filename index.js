8d2147a52e2be9e8d37e4f10a3b6da584a3662dc
Olyana Mushe <olyanamushe@MacBook-Pro-2.local>
2021-12-04 13:08:34 -0500

    push to heroku

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const ejs= require("ejs");
 const app = new express();
 
@@ -13,18 +13,64 @@ app.get("/", (req, res) => {
 });
 
 app.get("/my_resume", (req, res) => {
-    res.render(`my_resume`);
+    res.render(`html/my_resume`);
 });
 
 app.get("/my_fun_facts", (req, res) => {
-    res.render(`my_fun_facts`);
+    res.render(`html/my_fun_facts`);
 });
 
 app.get("/trivia", (req, res) => {
     res.render(`trivia`);
 });
 
+app.get('/trivia', async (req,res)=>{
+    var answer=req.query.keyword;
+    console.log(req.query.keyword);
+});
 
-//setting up server ports
-const PORT= process.env.PORT || 2000;
-app.listen(PORT, ()=> console.log(`Server is running on port 2000`)); 
+app.post('/submitquestion1',function(req, res){
+    var answer1= req.body.qOneAnswer; 
+    if(req.param.answer1==="b"){
+        res.send('CORRECT')
+    }
+    else res.send('incorrect');
+}); 
+
+app.post('/submitquestion2',function(req, res){
+    var answer2= req.body.qTwoAnswer; 
+    if(req.param.answer1==="c"){
+        res.send('CORRECT')
+    }
+    else res.send('incorrect');
+}); 
+
+app.post('/submitquestion3',function(req, res){
+    var answer3= req.body.qThreeAnswer; 
+    if(req.param.answer1==="a"){
+        res.send('CORRECT')
+    }
+    else res.send('incorrect');
+}); 
+
+app.post('/submitquestion4',function(req, res){
+    var answer4= req.body.qFourAnswer; 
+    if(req.param.answer1==="b"){
+        res.send('CORRECT')
+    }
+    else res.send('incorrect');
+}); 
+
+app.post('/submitquestion5',function(req, res){
+    var answer5= req.body.qFiveAnswer; 
+    if(req.param.answer1==="b"){
+        res.send('CORRECT')
+    }
+    else res.send('incorrect');
+}); 
+
+
+    
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
